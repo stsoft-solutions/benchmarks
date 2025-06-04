eaa135e184d5ad2513b91337f7a71fb5ca4ccbcc
stsoft-solutions <58132574+stsoft-solutions@users.noreply.github.com>
2025-06-04 12:40:08 +0300

    Add concurrency test for LockFreeStringPool

---
+++ string_pool/StringPoolTests/ConcurrentGetIdTests.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+using StringPoolBenchmark;
+
+namespace StringPoolTests;
+
+[UsedImplicitly]
+public class ConcurrentGetIdTests
+{
+    [Fact]
+    public void GetId_ShouldReturnSameId_WhenCalledFromMultipleThreads()
+    {
+        const int threadCount = 8;
+        const string value = "concurrent";
+        var pool = new LockFreeStringPool();
+        var ids = new int[threadCount];
+        var threads = new Thread[threadCount];
+
+        for (var i = 0; i < threadCount; i++)
+        {
+            var index = i;
+            threads[i] = new Thread(() => ids[index] = pool.GetId(value));
+            threads[i].Start();
+        }
+
+        foreach (var t in threads)
+            t.Join();
+
+        for (var i = 1; i < threadCount; i++)
+            Assert.Equal(ids[0], ids[i]);
+
+        Assert.True(pool.TryGetString(ids[0], out var retrieved));
+        Assert.Equal(value, retrieved);
+    }
+}
